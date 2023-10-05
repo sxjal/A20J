3cf2d1087eeb52264ce5a656e24a2308539dc0cf
SAJAL <72509355+sxjal@users.noreply.github.com>
2023-10-05 10:59:43 +0530

    Create 1_youngphy.cpp

---
+++ 11 - Codeforces Rating < 130/1_youngphy.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+	vector<int> input;
+
+	int n,k;
+	cin>>n;
+	k = 0;
+	int result = 0;
+	for(int i = 0 ; i < n ; i++)
+	{
+		 
+		for(int j = 0 ; j<3 ; j++)
+		{
+
+		cin>>k;
+		result +=k;
+
+		}
+	
+	}
+
+ result == 0 ? cout<<"YES\n" : cout<<"NO\n";
+
+	
+	return 0;
+}
